164f634433ebe0c6fba75baa23b13f304173b51d
Jesse205 <2055675594@qq.com>
2025-06-15 05:10:39 +0800

    refactor: 调整 pageSizeNameToCm 函数中的页面尺寸顺序，并更新 getAvailablePageSizes 函数以包含所有可用页面尺寸

--- src/features/print/utils/paperSize.ts
+++ src/features/print/utils/paperSize.ts
@@ -2,10 +2,16 @@ import type { DataType } from 'csstype';
 
 export function pageSizeNameToCm(pageSize: DataType.PageSize): [number, number] {
   switch (pageSize) {
-    case 'A4':
-      return [21.0, 29.7];
     case 'A3':
       return [29.7, 42.0];
+    case 'A4':
+      return [21.0, 29.7];
+    case 'A5':
+      return [14.8, 21.0];
+    case 'B4':
+      return [25.0, 35.3];
+    case 'B5':
+      return [17.6, 25.0];
     case 'letter':
       return [21.59, 27.94];
     case 'legal':
@@ -16,5 +22,5 @@ export function pageSizeNameToCm(pageSize: DataType.PageSize): [number, number] 
 }
 
 export function getAvailablePageSizes(): DataType.PageSize[] {
-  return ['A4', 'A3', 'letter', 'legal'];
+  return ['A3', 'A4', 'A5', 'B4', 'B5', 'letter', 'legal'];
 }
