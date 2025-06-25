0876881b2d306776e068b111de281a8bf9d32002
Jesse205 <2055675594@qq.com>
2025-06-25 10:56:42 +0800

    refactor(PrinterPage): 将自定义边距输入功能抽离为单独的组件
    - 新增 CustomMarginItems 组件用于自定义边距输入
    - 从 AdjustSheets 组件中移除相关的逻辑和 UI
    - 优化代码结构，提高组件的可维护性和可重用性

---
+++ src/pages/PrinterPage/components/AdjustSheets/CustomMarginItems.tsx
@@ -0,0 +1,139 @@
+import { InputAdornment, ListItem, TextField } from '@suid/material';
+import type { Accessor } from 'solid-js';
+import { type SyncStateOptions, syncState } from '@/hooks/syncState';
+import { useConfig } from '../../contexts/ConfigContext';
+
+function createMarginInputLinter(value: Accessor<string>) {
+  return () => {
+    const columns = Number.parseFloat(value());
+    if (Number.isNaN(columns) || columns < 0) {
+      return '请输入大于0的数字';
+    }
+    return null;
+  };
+}
+
+const MARGIN_INPUT_SYNC_STATE_OPTIONS: SyncStateOptions<number, string> = {
+  flowDown: (value) => String(value),
+  flowUp: (value) => Number.parseFloat(value),
+  canFlowUp: (value) => Number.parseFloat(value) > 0,
+};
+export default function CustomMarginItems() {
+  const { state: config, actions: configActions } = useConfig();
+  const [marginTopValue, setMarginTopValue] = syncState(
+    () => config.print.contentMarginTop,
+    (value) => configActions.print.setMarginTop(value()),
+    MARGIN_INPUT_SYNC_STATE_OPTIONS,
+  );
+  const [marginBottomValue, setMarginBottomValue] = syncState(
+    () => config.print.contentMarginBottom,
+    (value) => configActions.print.setMarginBottom(value()),
+    MARGIN_INPUT_SYNC_STATE_OPTIONS,
+  );
+  const [marginLeftValue, setMarginLeftValue] = syncState(
+    () => config.print.contentMarginLeft,
+    (value) => configActions.print.setMarginLeft(value()),
+    MARGIN_INPUT_SYNC_STATE_OPTIONS,
+  );
+  const [marginRightValue, setMarginRightValue] = syncState(
+    () => config.print.contentMarginRight,
+    (value) => configActions.print.setMarginRight(value()),
+    MARGIN_INPUT_SYNC_STATE_OPTIONS,
+  );
+
+  const marginTopError = createMarginInputLinter(marginTopValue);
+  const marginBottomError = createMarginInputLinter(marginBottomValue);
+  const marginLeftError = createMarginInputLinter(marginLeftValue);
+  const marginRightError = createMarginInputLinter(marginRightValue);
+
+  return (
+    <>
+      <ListItem
+        sx={{
+          alignItems: 'flex-start',
+        }}
+      >
+        <TextField
+          label="上边距"
+          type="number"
+          fullWidth
+          inputProps={{
+            min: 0,
+            step: 0.1,
+          }}
+          required
+          value={marginTopValue()}
+          onChange={(_event, value) => setMarginTopValue(value)}
+          error={marginTopError() !== null}
+          helperText={marginTopError()}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="下边距"
+          type="number"
+          fullWidth
+          inputProps={{
+            min: 0,
+            step: 0.1,
+          }}
+          sx={{
+            marginLeft: 2,
+          }}
+          required
+          value={marginBottomValue()}
+          onChange={(_event, value) => setMarginBottomValue(value)}
+          error={marginBottomError() !== null}
+          helperText={marginBottomError()}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+          }}
+        />
+      </ListItem>
+      <ListItem
+        sx={{
+          alignItems: 'flex-start',
+        }}
+      >
+        <TextField
+          label="左边距"
+          type="number"
+          fullWidth
+          inputProps={{
+            min: 0,
+            step: 0.1,
+          }}
+          required
+          value={marginLeftValue()}
+          onChange={(_event, value) => setMarginLeftValue(value)}
+          error={marginLeftError() !== null}
+          helperText={marginLeftError()}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="右边距"
+          type="number"
+          fullWidth
+          inputProps={{
+            min: 0,
+            step: 0.1,
+          }}
+          sx={{
+            marginLeft: 2,
+          }}
+          required
+          value={marginRightValue()}
+          onChange={(_event, value) => setMarginRightValue(value)}
+          error={marginRightError() !== null}
+          helperText={marginRightError()}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+          }}
+        />
+      </ListItem>
+    </>
+  );
+}
--- src/pages/PrinterPage/components/AdjustSheets/index.tsx
+++ src/pages/PrinterPage/components/AdjustSheets/index.tsx
@@ -8,7 +8,6 @@ import {
   FormControlLabel,
   FormHelperText,
   FormLabel,
-  InputAdornment,
   InputLabel,
   List,
   ListItem,
@@ -36,8 +35,9 @@ import { usePreferredDarkMode } from '@/hooks/mediaQuery';
 import { type SyncStateOptions, syncState } from '@/hooks/syncState';
 import { maybeAndroid, maybeChrome } from '@/utils/platform';
 import { mergeMultiSxProps } from '@/utils/suid';
-import { useConfig } from '../contexts/ConfigContext';
-import type { PrintConfig } from '../types/config';
+import { useConfig } from '../../contexts/ConfigContext';
+import type { PrintConfig } from '../../types/config';
+import CustomMarginItems from './CustomMarginItems';
 
 function createPositiveIntegerInputLinter(value: Accessor<string>) {
   return createMemo(() => {
@@ -48,26 +48,11 @@ function createPositiveIntegerInputLinter(value: Accessor<string>) {
     return null;
   });
 }
-function createMarginInputLinter(value: Accessor<string>) {
-  return createMemo(() => {
-    const columns = Number.parseFloat(value());
-    if (Number.isNaN(columns) || columns < 0) {
-      return '请输入大于0的数字';
-    }
-    return null;
-  });
-}
 
 const POSITIVE_INTEGER_INPUT_SYNC_STATE_OPTIONS: SyncStateOptions<number, string> = {
   flowDown: (value) => String(value),
   flowUp: (value) => Number.parseInt(value),
   canFlowUp: (value) => Number.parseInt(value) > 0,
-};
-
-const MARGIN_INPUT_SYNC_STATE_OPTIONS: SyncStateOptions<number, string> = {
-  flowDown: (value) => String(value),
-  flowUp: (value) => Number.parseFloat(value),
-  canFlowUp: (value) => Number.parseFloat(value) > 0,
 };
 
 interface AdjustSheetsProps extends DrawerProps {}
@@ -109,32 +94,6 @@ export default function AdjustSheets(props: AdjustSheetsProps) {
   function handleMarginPresetChange(event: SelectChangeEvent<PrintConfig['contentMarginPreset']>, _child: JSX.Element) {
     configActions.print.setMarginPreset(event.target.value);
   }
-
-  const [marginTopValue, setMarginTopValue] = syncState(
-    () => config.print.contentMarginTop,
-    (value) => configActions.print.setMarginTop(value()),
-    MARGIN_INPUT_SYNC_STATE_OPTIONS,
-  );
-  const [marginBottomValue, setMarginBottomValue] = syncState(
-    () => config.print.contentMarginBottom,
-    (value) => configActions.print.setMarginBottom(value()),
-    MARGIN_INPUT_SYNC_STATE_OPTIONS,
-  );
-  const [marginLeftValue, setMarginLeftValue] = syncState(
-    () => config.print.contentMarginLeft,
-    (value) => configActions.print.setMarginLeft(value()),
-    MARGIN_INPUT_SYNC_STATE_OPTIONS,
-  );
-  const [marginRightValue, setMarginRightValue] = syncState(
-    () => config.print.contentMarginRight,
-    (value) => configActions.print.setMarginRight(value()),
-    MARGIN_INPUT_SYNC_STATE_OPTIONS,
-  );
-
-  const marginTopError = createMarginInputLinter(marginTopValue);
-  const marginBottomError = createMarginInputLinter(marginBottomValue);
-  const marginLeftError = createMarginInputLinter(marginLeftValue);
-  const marginRightError = createMarginInputLinter(marginRightValue);
 
   const isPreferredDark = usePreferredDarkMode();
 
@@ -278,84 +237,7 @@ export default function AdjustSheets(props: AdjustSheetsProps) {
             </FormControl>
           </ListItem>
           <Show when={config.print.contentMarginPreset === 'custom'}>
-            <ListItem>
-              <TextField
-                label="上边距"
-                type="number"
-                fullWidth
-                inputProps={{
-                  min: 0,
-                  step: 0.1,
-                }}
-                required
-                value={marginTopValue()}
-                onChange={(_event, value) => setMarginTopValue(value)}
-                error={marginTopError() !== null}
-                helperText={marginTopError()}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">cm</InputAdornment>,
-                }}
-              />
-              <TextField
-                label="下边距"
-                type="number"
-                fullWidth
-                inputProps={{
-                  min: 0,
-                  step: 0.1,
-                }}
-                sx={{
-                  marginLeft: 2,
-                }}
-                required
-                value={marginBottomValue()}
-                onChange={(_event, value) => setMarginBottomValue(value)}
-                error={marginBottomError() !== null}
-                helperText={marginBottomError()}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">cm</InputAdornment>,
-                }}
-              />
-            </ListItem>
-            <ListItem>
-              <TextField
-                label="左边距"
-                type="number"
-                fullWidth
-                inputProps={{
-                  min: 0,
-                  step: 0.1,
-                }}
-                required
-                value={marginLeftValue()}
-                onChange={(_event, value) => setMarginLeftValue(value)}
-                error={marginLeftError() !== null}
-                helperText={marginLeftError()}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">cm</InputAdornment>,
-                }}
-              />
-              <TextField
-                label="右边距"
-                type="number"
-                fullWidth
-                inputProps={{
-                  min: 0,
-                  step: 0.1,
-                }}
-                sx={{
-                  marginLeft: 2,
-                }}
-                required
-                value={marginRightValue()}
-                onChange={(_event, value) => setMarginRightValue(value)}
-                error={marginRightError() !== null}
-                helperText={marginRightError()}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">cm</InputAdornment>,
-                }}
-              />
-            </ListItem>
+            <CustomMarginItems />
           </Show>
           <ListItem>
             <FormControl fullWidth variant="standard">
