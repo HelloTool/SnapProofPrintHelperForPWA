1049b06838e237ad1111a8e8cccaeecd5f5651ee
Jesse205 <2055675594@qq.com>
2025-06-27 17:16:17 +0800

    feat(makeMeta): 优化 meta 元素的创建和移除逻辑
    - 改进 meta 元素的创建方式，确保每次调用时只创建一次
    - 添加组件卸载时移除 meta 元素的逻辑，避免内存泄漏
    - 优化 createEffect 中的更新逻辑，增加对 meta 元素存在性的判断

--- src/hooks/makeMeta.ts
+++ src/hooks/makeMeta.ts
@@ -1,14 +1,17 @@
-import { type Accessor, createEffect } from 'solid-js';
+import { type Accessor, createEffect, onCleanup } from 'solid-js';
 
 export function makeMeta(name: string, content: Accessor<string>) {
-  let metaElement = document.querySelector<HTMLMetaElement>(`meta[name="${name}"]`);
-  if (!metaElement) {
-    metaElement = document.createElement('meta');
-    metaElement.name = name;
-    document.head.appendChild(metaElement);
-  }
+  let metaElement: HTMLMetaElement | undefined = document.createElement('meta');
+  metaElement.name = name;
+  document.head.appendChild(metaElement);
+  onCleanup(() => {
+    if (!metaElement) return;
+    document.head.removeChild(metaElement);
+    metaElement = undefined;
+  });
   // Meta 组件不稳定，所以需要手动更新
   createEffect(() => {
+    if (!metaElement) return;
     metaElement.content = content();
   });
 }
